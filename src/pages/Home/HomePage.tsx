1fe2dce817e170b2fa58d6c5e6cb10356f9f3856
ErenKarakaya <erenkarakaya@eskisehir.edu.tr>
2024-02-02 11:33:49 +0300

    text center fixes

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -6,8 +6,8 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className="text-4xl font-bold mb-8">Welcome to the Quiz</h1>
-      <p className="text-lg mb-4">
+      <h1 className="text-4xl text-center font-bold mb-8">Welcome to the Quiz</h1>
+      <p className="text-lg text-center mb-4">
         Test your knowledge with our quiz. Answer questions and see how well you
         score!
       </p>
